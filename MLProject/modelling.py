6393dab3f941fc1a369df7cd676ad70034c88795
nrainiz <nurainisa28@gmail.com>
2025-12-29 20:29:10 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,37 @@
+import mlflow
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import os
+import numpy as np
+import warnings
+import sys
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(42)
+
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    dataset_path = sys.argv[2] if len(sys.argv) > 2 else "Croprecommendation_preprocessing.csv"
+
+    data = pd.read_csv(dataset_path)
+
+    X = data.drop("label", axis=1)
+    y = data["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+    input_example = X_train[0:5]
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators)
+        model.fit(X_train, y_train)
+
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example
+        )
+
+        mlflow.log_param("n_estimators", n_estimators)
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
